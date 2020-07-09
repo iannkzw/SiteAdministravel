1d0bb307a414b16f96d78b90a82de8da0105ffcd
Ian Nakazawa <iannkzw@gmail.com>
2020-07-08 21:23:52 -0300

    upload image

--- templates/admin.tpl.php
+++ templates/admin.tpl.php
@@ -45,13 +45,46 @@
 </div>
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <script src="/resources/trix/trix.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pnotify/3.0.0/pnotify.min.js"></script>
 
 <script>
+    document.addEventListener('trix-attachment-add', function () {
+        const attachment = event.attachment;
+        if (!attachment.file)
+        {
+            return;
+        }
+        const form = new FormData();
+        form.append('file', attachment.file);
+
+        $.ajax({
+            url: '/admin/upload/image',
+            method: 'POST',
+            data: form,
+            contentType: false,
+            processData: false,
+            xhr: function () {
+                const xhr = $.ajaxSettings.xhr();
+                xhr.upload.addEventListener('progress', function (e) {
+                    let  progress = e.loaded / e.total * 100;
+                    attachment.setUploadProgress(progress)
+                })
+                return xhr;
+            }
+        }).done(function (response) {
+            attachment.setAttributes({
+                url: response,
+                href: response
+            })
+        }).fail(function () {
+            console.log('errado')
+        })
+    });
+
     <?php flash(); ?>
 
     const confirmEl = document.querySelector('.confirm');
